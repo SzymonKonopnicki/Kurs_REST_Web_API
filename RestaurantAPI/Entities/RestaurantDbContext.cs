c458226c8d7908d9d276848c89bd3942f1d9db07
Czarno Mysz <szymonkonopnicki@outlook.com>
2023-12-14 18:35:25 +0100

    Creating Migration for DB

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        //TODO: move connection string to .json file
+        private string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=RestaurantApiDB;Trusted_Connection=True;";
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(x => x.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
