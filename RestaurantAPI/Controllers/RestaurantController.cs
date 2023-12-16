4fc6e1e8be4c256affb285cf107674669ad2601d
Czarno Mysz <szymonkonopnicki@outlook.com>
2023-12-16 20:17:18 +0100

    Created controller with Get attributes

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models.Dtos;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private RestaurantDbContext _dbContext;
+        private IMapper _mapper;
+
+        public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAllRestaurants()
+        {
+            var restaurants = _dbContext.Restaurants
+                .Include(x => x.Dish)
+                .Include(x => x.Address)
+                .ToList();
+
+            var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+
+            return Ok(restaurantsDto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<RestaurantDto> GetRestaurantById(int id)
+        {
+            var restaurant = _dbContext.Restaurants
+                .Include(x => x.Dish)
+                .Include(x => x.Address)
+                .FirstOrDefault(x => x.Id == id);
+
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            var resyaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+
+            return Ok(resyaurantDto);
+        }
+
+    }
+}
